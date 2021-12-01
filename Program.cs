960fc23e3a9dd28ea5614c24fc5eabe2e2d55ca3
shalini Kumarii <bittukumari@gmail.com>
2021-12-01 10:10:51 -0800

    Deck of card first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DeckOfCard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Deck deck = new Deck ();
+
+            Player riker = new Player("Riker");
+
+            deck.Shuffle();
+
+            deck.ReadDeck();
+
+            riker.Draw(deck, riker);
+            riker.Draw(deck, riker);
+            riker.Draw(deck, riker);
+            riker.Draw(deck, riker);
+            riker.Draw(deck, riker);
+
+            System.Console.WriteLine(riker.Hand);
+            deck.ReadDeck();
+        }
+    }
+}
